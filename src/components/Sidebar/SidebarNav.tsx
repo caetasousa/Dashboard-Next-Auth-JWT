ac925910ec6eaf0c47561f7c535065279ed22139
caetasousa <caetasousa@gmail.com>
2021-10-14 15:03:24 -0300

    Adicionando os caminhos de navegação

--- src/components/Sidebar/SidebarNav.tsx
+++ src/components/Sidebar/SidebarNav.tsx
@@ -7,8 +7,8 @@ export function SidebarNav() {
     return(
         <Stack spacing="12" align="flex-start">
             <NavSection title="GERAL">
-                <NavLink icon={RiDashboardLine}>Dashboard</NavLink>
-                <NavLink icon={RiContactsLine}>Usuarios</NavLink>
+                <NavLink icon={RiDashboardLine} href="/dashboard">Dashboard</NavLink>
+                <NavLink icon={RiContactsLine} href="/users">Usuarios</NavLink>
             </NavSection>
         </Stack>
     );
