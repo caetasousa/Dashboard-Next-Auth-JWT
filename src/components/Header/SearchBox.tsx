d8ac7c94beb4608fa3d893ea176643ae25b10b81
caetasousa <caetasousa@gmail.com>
2021-10-18 15:08:16 -0300

    Formulario de login

--- src/components/Header/SearchBox.tsx
+++ src/components/Header/SearchBox.tsx
@@ -1,9 +1,8 @@
 import { Flex, Icon, Input } from "@chakra-ui/react";
 import { RiSearchLine } from 'react-icons/ri'
 
-
-
 export function SearchBox() {
+
     return(
         <Flex
             as="label"
